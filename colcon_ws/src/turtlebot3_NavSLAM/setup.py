8ae3d48c5fafd99c65e1e81d250aabd33f815d50
nicolamarchiotto <ziapo98@gmail.com>
2022-05-04 17:16:04 +0200

    first commit

---
+++ colcon_ws/src/turtlebot3_NavSLAM/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import os
+
+from glob import glob
+
+package_name = 'turtlebot3_NavSLAM'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'map'), glob('map/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='trotti.francescovr@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
